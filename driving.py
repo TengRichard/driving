67f2a1bbb77cd4422c67626771fe8700b50f0eab
Teng Richard <teng.richard@gmail.com>
2019-12-16 11:54:10 +0800

    add USA

--- driving.py
+++ driving.py
@@ -1,8 +1,14 @@
 contry = input('你是哪國人:  ')
-age = input('輸入年齡')
+age = input('輸入年齡: ')
 age = int(age)
 if contry == '台灣':
     if age >= 18:
     	print('你可以考駕照:  ')
     else:
     	print('你還不能考駕照: ')
+elif contry == '美國':
+	if age >= 16:
+		print('你可以考駕照:  ')
+	else:
+		print('你還不能考駕照')
+
